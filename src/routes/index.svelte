9da4b76faf99d357e4531a7f9076266a943c072d
demaiodev <fml1041@gmail.com>
2022-08-15 15:42:34 -0400

    type dieFaces as any

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,17 +21,7 @@
 		wins: number;
 	};
 
-	type DieFaces = {
-		0: string;
-		1: string;
-		2: string;
-		3: string;
-		4: string;
-		5: string;
-		6: string;
-	};
-
-	const dieFaces: DieFaces = {
+	const dieFaces: any = {
 		0: '',
 		1: '\u2680',
 		2: '\u2681',
