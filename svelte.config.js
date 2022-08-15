c1c71eedbe2de942295e1c8579ff5bf99adffeac
demaiodev <fml1041@gmail.com>
2022-08-15 11:49:22 -0400

    svelte-add bootstrap

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,13 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@use "src/variables.scss" as *;'
+			}
+		})
+	],
 
 	kit: {
 		adapter: adapter(),
