e11f934b6e76fc7d53f1e65a4e2caea5656cc08d
demaiodev <fml1041@gmail.com>
2022-08-26 12:07:42 -0400

    finish up match history page

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	let matches: [] = [];
+	let matches: any[] = [];
+
 	onMount(async () => {
 		matches = JSON.parse(localStorage.getItem('matches')!) || [];
 	});
+
+	function getGameStatus(p1score: number, p2score: number) {
+		if (p1score > p2score) return 'You won this one!';
+		else if (p1score === p2score) return 'You tied this one somehow. Cool!';
+		else return 'You lost this one.';
+	}
 </script>
 
 <section>
 	<h1 class="my-4 pb-4">Match History</h1>
 	{#if matches.length}
-		{#each matches as match}
-			<p>{match}</p>
+		{#each matches as match, index (index)}
+			<div class="container border p-3">
+				<h3 class="pb-3">Game #{index + 1}</h3>
+				<p>{match.playerOne.name}: <span class="p1">{match.playerOne.score}</span></p>
+				<p>{match.playerTwo.name}: <span class="p2">{match.playerTwo.score}</span></p>
+				<p>{getGameStatus(match.playerOne.score, match.playerTwo.score)}</p>
+			</div>
 		{/each}
 	{:else}
 		<p class="text-center">
@@ -19,3 +31,12 @@
 		</p>
 	{/if}
 </section>
+
+<style lang="scss">
+	.p1 {
+		color: var(--accent-color);
+	}
+	.p2 {
+		color: var(--accent-color-secondary);
+	}
+</style>
