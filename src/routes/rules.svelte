7477b1127c1bdb9d1291b422db70b0127adc5a72
tyler langties <tylerlangties@gmail.com>
2022-08-26 12:44:38 -0400

    make rules responsive

--- src/routes/rules.svelte
+++ src/routes/rules.svelte
@@ -2,11 +2,11 @@
 	<h1 class="my-4 pb-4">How To Play</h1>
 	<h5>Your score is calculated by adding all of your dice together.</h5>
 	<div class="parent container my-3">
-		<div class="my-4 div1 pt-4">
+		<div class="my-4 pt-4">
 			<h3>Match Dice</h3>
 			<p>When dice of the same number are placed in the same column, multiply their value.</p>
 		</div>
-		<div class="my-4 div2 pt-4">
+		<div class="my-4 pt-4">
 			<h3>Destroy Opponent</h3>
 			<p>Destroy your opponent's dice by matching yours to theirs.</p>
 		</div>
@@ -18,12 +18,11 @@
 		display: grid;
 		grid-template-columns: repeat(2, 1fr);
 		grid-template-rows: repeat(2, 1fr);
-	}
-
-	.div1 {
-		grid-area: 1 / 2 / 2 / 1;
-	}
-	.div2 {
-		grid-area: 1 / 2 / 2 / 4;
+		@media only screen and (max-width: 768px) {
+			display: grid;
+			grid-template-columns: 1fr;
+			margin: 0;
+			grid-template-rows: 1fr;
+		}
 	}
 </style>
