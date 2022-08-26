7d2aa32aec8f482891a9fe941a84c5efb6f59514
demaiodev <fml1041@gmail.com>
2022-08-26 11:48:29 -0400

    begin match history logic

--- src/routes/history.svelte
+++ src/routes/history.svelte
@@ -1,3 +1,21 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let matches: [] = [];
+	onMount(async () => {
+		matches = JSON.parse(localStorage.getItem('matches')!) || [];
+	});
+</script>
+
 <section>
 	<h1 class="my-4 pb-4">Match History</h1>
+	{#if matches.length}
+		{#each matches as match}
+			<p>{match}</p>
+		{/each}
+	{:else}
+		<p class="text-center">
+			There is no match history for you yet! <br /> Go play <a href="/">here</a>.
+		</p>
+	{/if}
 </section>
